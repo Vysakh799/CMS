a241dab11d61796b4181753207e2cc4e814ad6c0
Vysakh <vysakh7995@gmail.com>
2024-08-25 21:31:27 +0530

    adding all login

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index),
+
+
+    #Student
     path('st_login',views.st_login),
+
+    #Staff
+    path('staff',views.staff_login),
+
+    #Admin
+    path('admin',views.admin_login),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -6,4 +6,10 @@ def index(request):
     return render(request,'index.html')
 
 def st_login(request):
-    return render(request,'student_login.html')
+    return render(request,'student/student_login.html')
+
+def staff_login(request):
+    return render(request,"staff/staff_login.html")
+
+def admin_login(request):
+    return render(request,'admin/admin_login.html')
